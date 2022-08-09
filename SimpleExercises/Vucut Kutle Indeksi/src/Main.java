033bd32e05579b62492e7d9efcc74d3867315fff
Ahmet <ahmet.yetimoglu0@gmail.com>
2022-08-09 10:33:45 +0300

    Vücut Kütle indeksi

---
+++ SimpleExercises/Vucut Kutle Indeksi/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double boy;
+        int kilo;
+        double sonuc;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = input.nextInt();
+
+        sonuc = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz : " + sonuc);
+    }
+}
