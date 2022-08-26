9cda4d95bab9684790a1222e63e9cb6c2ccb561f
Ahmet <ahmet.yetimoglu0@gmail.com>
2022-08-26 12:26:13 +0300

    Us Hesabi

--- SimpleExercises/Metotlar/Us Hesabi/src/Main.java
+++ SimpleExercises/Metotlar/Us Hesabi/src/Main.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    static void expo() {
+    static int expo(int tabanDegeri, int usDegeri,int sonuc) {
+        if(usDegeri == 0){
+            return sonuc;
+        }
+        sonuc = sonuc*tabanDegeri;
+        return expo(tabanDegeri,usDegeri-1,sonuc);
+    }
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Taban değeri giriniz : ");
         int tabanDegeri = scan.nextInt();
         System.out.print("Üs değeri giriniz : ");
         int usDegeri = scan.nextInt();
         int sonuc = 1;
-        for (int i=0;i<usDegeri;i++) {
-            sonuc *= tabanDegeri;
-        }
-        System.out.println("Sonuc : " +sonuc);
-    }
-    public static void main(String[] args) {
-        expo();
+        System.out.println(expo(tabanDegeri,usDegeri,sonuc));
     }
 }
